4571377ede4c15d7cf5df7445b06d19fa9eb1fe1
D34dsec <james.sawkins@googlemail.com>
2023-04-02 23:06:09 +0100

    Add separate input for seconds and usage instructions

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.getElementById("cooking-time-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const originalTime = parseFloat(document.getElementById("original-time").value);
+    const originalTimeMinutes = parseFloat(document.getElementById("originalTimeMinutes").value) || 0;
+    const originalTimeSeconds = parseFloat(document.getElementById("originalTimeSeconds").value) || 0;
+    const originalTime = (originalTimeMinutes * 60) + originalTimeSeconds;
     const originalWattage = parseInt(document.getElementById("original-wattage").value);
     const newWattage = parseInt(document.getElementById("new-wattage").value);
 
     const newTime = (originalTime * originalWattage) / newWattage;
-    const minutes = Math.floor(newTime);
-    const seconds = Math.round((newTime - minutes) * 60);
+    const minutes = Math.floor(newTime / 60);
+    const seconds = Math.round(newTime % 60);
 
     document.getElementById("result").textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 });
