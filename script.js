15a22de96ec32eb5568f5c041bf063c810ed3e91
D34dsec <james.sawkins@googlemail.com>
2023-04-03 00:33:47 +0100

    Add separate input for seconds and usage instructions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 document.getElementById("cooking-time-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const originalTimeMinutes = parseFloat(document.getElementById("originalTimeMinutes").value) || 0;
-    const originalTimeSeconds = parseFloat(document.getElementById("originalTimeSeconds").value) || 0;
-    const originalTime = (originalTimeMinutes * 60) + originalTimeSeconds;
-    const originalWattage = parseInt(document.getElementById("original-wattage").value);
-    const newWattage = parseInt(document.getElementById("new-wattage").value);
+    if (this.checkValidity()) {
+        const originalTimeMinutes = parseFloat(document.getElementById("originalTimeMinutes").value) || 0;
+        const originalTimeSeconds = parseFloat(document.getElementById("originalTimeSeconds").value) || 0;
+        const originalTime = (originalTimeMinutes * 60) + originalTimeSeconds;
+        const originalWattage = parseInt(document.getElementById("original-wattage").value);
+        const newWattage = parseInt(document.getElementById("new-wattage").value);
 
-    const newTime = (originalTime * originalWattage) / newWattage;
-    const minutes = Math.floor(newTime / 60);
-    const seconds = Math.round(newTime % 60);
+        const newTime = (originalTime * originalWattage) / newWattage;
+        const minutes = Math.floor(newTime / 60);
+        const seconds = Math.round(newTime % 60);
 
-    document.getElementById("result").textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+        document.getElementById("result").textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    } else {
+        this.classList.add('was-validated');
+    }
 });
